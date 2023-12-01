7521dab444305e96159cc6a4c2d83ed3d27578f5
Khushali-kavathiya <kavathiyakhushali@gmail.com>
2023-12-01 21:01:59 +0530

    "Edit"

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-var inputvalue = document.querySelector('#cityinput')
-var btn = document.querySelector('#add')
-var city = document.querySelector('#cityoutput')
-var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
-var wind = document.querySelector('#wind')
-
-apik="7715e3f9b4e0b5b79e97224ed02fcb54"
-
-function convertion(val)
-{
-    return (val - 273).toFixed(3)
-}
-
-btn.addEventListener('click',function()
-{
-    fetch('https://api.openweathermap.org/data/2.5/wether?q='+inputvalue.value+'&appid='+apik).then(res => res.json())
-
-    .then(data =>
-        {
-            var nameval = data['name']
-            var descrip = data['wether']['0']['description']
-            var tempature = data['main']['temp']
-            var wndspeed = data['wind']['speed']
-
-            city.innerHTML = 'Wether of <span>${nameval}<span>'
-            temp.innerHTML = 'Tempature: <span>${convertion(tempature)} C</span>'
-            description.innerHTML = 'sky Conditions: <span>${descrip}<span>'
-            wind.innerHTML = 'Wind speed: <span>${wndspeed} km/h<span>'
-        }
+let weather = {
+    "apikey": "22f60a6da558d4fefeee13dd195fdaf8",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        + "&units=matric&appid="
+        + this.apikey
         )
+        .then((Response) => Response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon, description,temp, humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " +name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+       console.log("des", description)
+         //document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = description+'  '+temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: "+humidity +"%";
+        document.querySelector(".wind").innerText = "wind speed: "+speed +"km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        // document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1600x900/?" + name + "')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
 
-        .catch(err => alert('you entered wrong city name'))
-}
-)
+};
+
+document
+.querySelector(".search button")
+.addEventListener("click", function() {
+    weather.search();
+
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Denver");
+
